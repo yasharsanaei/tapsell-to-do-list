7eae4851d817fe3747423e676b3db7b59753be42
yasharsanayee <yasharsanayee@gmail.com>
2024-01-05 11:45:40 +0330

    view list component to show a list

---
+++ src/app/components/view-list/view-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-list.component.html',
+  styleUrl: './view-list.component.css'
+})
+export class ViewListComponent {
+
+}
