18fa0d3ec197f6d186bbb9f1e37f2f96324d2ad3
yasharsanayee <yasharsanayee@gmail.com>
2024-01-05 11:45:18 +0330

    basic layout created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ViewListComponent } from './components/view-list/view-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: ':id',
+        component: ViewListComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+];
