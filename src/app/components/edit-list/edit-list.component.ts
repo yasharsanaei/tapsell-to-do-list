5c6c61060657be7b2a15e3e278e3e4f980d3b4b9
yasharsanayee <yasharsanayee@gmail.com>
2024-01-06 18:00:56 +0330

    edit list component created

---
+++ src/app/components/edit-list/edit-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-list.component.html',
+  styleUrl: './edit-list.component.css'
+})
+export class EditListComponent {
+
+}
