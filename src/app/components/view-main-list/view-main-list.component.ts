0b1c0a2fda498e4a25a0dadbc7d4c11ad43b3c7c
yasharsanayee <yasharsanayee@gmail.com>
2024-01-07 17:26:42 +0330

    main and other lists view

--- src/app/components/view-main-list/view-main-list.component.ts
+++ src/app/components/view-main-list/view-main-list.component.ts
@@ -2,11 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { ListService } from '../../services/list.service';
 import { JsonPipe } from '@angular/common';
+import {MatDividerModule} from "@angular/material/divider";
+import {DeleteConfirmationDirective} from "../../utils/directives/delete-confirmation.directive";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-view-main-list',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [
+    JsonPipe,
+    MatDividerModule,
+    DeleteConfirmationDirective,
+    MatButtonModule,
+  ],
   templateUrl: './view-main-list.component.html',
   styleUrl: './view-main-list.component.css',
 })
@@ -15,6 +23,7 @@ export class ViewMainListComponent implements OnInit {
   #listService = inject(ListService);
 
   tasks = this.#taskService.tasks;
+  mainList = this.#listService.mainList;
 
   ngOnInit(): void {
     this.#listService.mainList.data$.subscribe((value) => {
