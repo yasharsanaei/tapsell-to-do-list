2967834c331ca7d38c66945cceae315db951b5a8
yasharsanayee <yasharsanayee@gmail.com>
2024-01-08 09:54:24 +0330

    task ui done

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { TaskDto } from '../../types/dto/task-dto';
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.css',
 })
